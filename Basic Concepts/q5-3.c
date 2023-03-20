43113a05ddccd02549208fe27d3f28f03335e411
thegr8od <127323247+thegr8od@users.noreply.github.com>
2023-03-20 18:02:57 +0900

    Rename q5-3.c to Basic Concepts/q5-3.c

--- Basic Concepts/q5-3.c
+++ Basic Concepts/q5-3.c
@@ -21,7 +21,7 @@ void sort(int arr[], int n)
     for (i = 1; i < n; i++) {
         key = arr[i];
         for (j = i - 1; j >= 0 && arr[j] > key; j--) {
-            arr[j + 1] = arr[j]; // �ڷ��̵�
+            arr[j + 1] = arr[j]; // µÚ·ÎÀÌµ¿
         }
         arr[j + 1] = key;
     }
